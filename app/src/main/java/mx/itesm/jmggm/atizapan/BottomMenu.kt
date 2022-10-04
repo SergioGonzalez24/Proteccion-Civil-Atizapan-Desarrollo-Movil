aafa90b9df75d4a08bfdbb871c171fddbdef7445
Gurtubay <josegurtubaymoreno@gmail.com>
2022-10-04 18:53:18 -0500

    Estilo Pantallas

--- app/src/main/java/mx/itesm/jmggm/atizapan/BottomMenu.kt
+++ app/src/main/java/mx/itesm/jmggm/atizapan/BottomMenu.kt
@@ -26,7 +26,10 @@ class BottomMenu : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.mainFragment,
+                R.id.newsFragment,
+                R.id.directoryFragment,
+                R.id.settingsFragment
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
