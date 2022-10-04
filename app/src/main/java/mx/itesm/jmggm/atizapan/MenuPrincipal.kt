fb38e4cc616ac3c80e95f65f60e94d27df7466fb
André <84719490+gggandre@users.noreply.github.com>
2022-10-04 15:54:23 -0500

    Menu profe

---
+++ app/src/main/java/mx/itesm/jmggm/atizapan/MenuPrincipal.kt
@@ -0,0 +1,44 @@
+package mx.itesm.jmggm.atizapan
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import mx.itesm.jmggm.atizapan.databinding.ActivityMenuPrincipalBinding
+
+class MenuPrincipal : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuPrincipalBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMenuPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_menu_principal)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                // id elementos menu que se necesiten
+                R.id.navigation_menu,
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_menu_principal)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
