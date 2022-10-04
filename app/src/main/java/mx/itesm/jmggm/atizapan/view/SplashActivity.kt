c056391623aca22336432023d332b9958af4225f
André <84719490+gggandre@users.noreply.github.com>
2022-10-04 17:49:07 -0500

    SE CIERRAAAAAAAAAAAA

---
+++ app/src/main/java/mx/itesm/jmggm/atizapan/view/SplashActivity.kt
@@ -0,0 +1,23 @@
+package mx.itesm.jmggm.atizapan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        handler = Handler()
+
+        handler.postDelayed({
+            val intent = Intent(this, MenuPrincipal::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
