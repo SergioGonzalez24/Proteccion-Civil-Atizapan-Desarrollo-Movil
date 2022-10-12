2f900aac72367d2e756221fa9d75f03560130940
André <84719490+gggandre@users.noreply.github.com>
2022-10-11 22:25:31 -0500

    NotifA

---
+++ app/src/main/java/mx/itesm/jmggm/atizapan/model/MyFirebaseMessagingService.kt
@@ -0,0 +1,78 @@
+package mx.itesm.jmggm.atizapan.model
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import mx.itesm.jmggm.atizapan.BottomMenu
+import mx.itesm.jmggm.atizapan.R
+
+const val CHANNEL_ID = "NOTIFICATION_CHANNEL"
+const val  CHANNEL_NAME = "mx.itesm.jmggm.atizapan"
+
+
+class MyFirebaseMessagingService :  FirebaseMessagingService()  {
+
+    //Generar notificacion
+    private fun generateNotification(title: String, message: String){
+        val intent = Intent(this, BottomMenu::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        var pendingIntent : PendingIntent
+        //var pendingIntent =   PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            pendingIntent  = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+        } else {
+            pendingIntent =   PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        }
+
+        //channel id, channel name
+        var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        //Attach la notificacion creada a un layout custom
+        builder = builder.setContent(getRemoteView(title, message))
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        //verificar si android es mayor a android Oreo
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        notificationManager.notify(0, builder.build())
+    }
+
+    @SuppressLint("RemoteViewLayout")
+    private fun getRemoteView(title: String, message: String) : RemoteViews {
+        val remoteView = RemoteViews("mx.itesm.jmggm.atizapan", R.drawable.ic_notifaction)
+        remoteView.setTextViewText(R.id.title, title)
+        remoteView.setTextViewText(R.id.message, message)
+        remoteView.setImageViewResource(R.id.image, R.drawable.ic_notifaction)
+
+        return remoteView
+    }
+
+    //mostrar la notificacion
+    override fun onMessageReceived(message: RemoteMessage) {
+        if(message.notification !=null){
+            generateNotification(message.notification!!.title!!, message.notification!!.body!!)
+        }
+    }
+
+
+}
