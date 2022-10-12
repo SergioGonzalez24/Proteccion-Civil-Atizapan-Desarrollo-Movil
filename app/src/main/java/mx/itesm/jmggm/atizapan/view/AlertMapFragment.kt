7d7a53b16eeefa2116d9b447bfab8a41fe7d4d86
André <84719490+gggandre@users.noreply.github.com>
2022-10-11 20:22:43 -0500

    Mapa con coordenadas

--- app/src/main/java/mx/itesm/jmggm/atizapan/view/AlertMapFragment.kt
+++ app/src/main/java/mx/itesm/jmggm/atizapan/view/AlertMapFragment.kt
@@ -18,6 +18,7 @@ import androidx.core.content.ContextCompat
 import androidx.navigation.navArgs
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import mx.itesm.jmggm.atizapan.BuildConfig
@@ -29,11 +30,6 @@ class AlertMapFragment : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMyL
     private lateinit var binding:ActivityAlertMapFragmentBinding
     private val args:AlertMapFragmentArgs by navArgs()
     private lateinit var map:GoogleMap
-    // Cliente proveedor de ubicación
-    private lateinit var clienteLocalizacion: FusedLocationProviderClient
-    // Callback para manejar las actualizaciones de ubicación
-    private lateinit var locationCallback: com.google.android.gms.location.LocationCallback
-    val CODIGO_PERMISO_GPS=200
     companion object{
         const val RESQUEST_CODE_LOCATION=0
     }
@@ -67,74 +63,22 @@ class AlertMapFragment : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMyL
         //setContentView(R.layout.activity_alert_map_fragment)
         ocultarImg()
         createMapFragment()
-        registrarEventos(position)
+        registrarEventos()
 
     }
 
-    private fun registrarEventos(position: Location) {
-            Toast.makeText(this, "Estas en ${position.latitude},${position.longitude}", Toast.LENGTH_SHORT).show()
+    private fun registrarEventos() {
+            binding.btnSolicitarServicio.setOnClickListener {
+                Toast.makeText(this,
+                    "Estas en ${position.latitude},${position.longitude}",
+                    Toast.LENGTH_SHORT).show()
+            }
     }
 
     override fun onStart() {
         super.onStart()
-        if (! this::clienteLocalizacion.isInitialized) {
-            clienteLocalizacion = LocationServices.getFusedLocationProviderClient(this)
-        }
         //registrarEventos(position)
     }
-
-    private fun configurarGPS() {
-        if (tienePermiso()) {
-            iniciarActualizacionesPosicion()
-        } else {
-            solicitarPermisos()
-        }
-    }
-
-    private fun solicitarPermisos() {
-        val requiereJustificacion = ActivityCompat.shouldShowRequestPermissionRationale(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-
-        if (requiereJustificacion) {
-            mostrarDialogo()
-        } else { // Pedir el permiso directo
-            pedirPermisoUbicacion()
-        }
-    }
-
-
-    private fun pedirPermisoUbicacion() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), CODIGO_PERMISO_GPS
-        )
-    }
-
-    private fun mostrarDialogo() {
-        val dialogo = AlertDialog.Builder(this)
-            .setMessage("Necesitamos saber tu posición para generar alertas")
-            .setPositiveButton("Aceptar") { dialog, which ->
-                pedirPermisoUbicacion()
-            }
-            .setNeutralButton("Cancelar", null)
-        dialogo.show()
-    }
-
-    private fun tienePermiso(): Boolean {
-        val estado = ContextCompat.checkSelfPermission(
-            applicationContext,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-        return estado == PackageManager.PERMISSION_GRANTED
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun iniciarActualizacionesPosicion() {
-
-    }
-
 
 
     @Suppress("UNREACHABLE_CODE", "UnusedEquals")
@@ -159,14 +103,6 @@ class AlertMapFragment : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMyL
         val mapFragment = supportFragmentManager.findFragmentById(R.id.AlertMap) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
-
-    /*
-    private fun createMapFragment() {
-        val mapFragment = supportFragmentManager.findFragmentById(com.google.android.gms.maps.R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-
-    }
-     */
 
     private fun isLocationPermissionGranted() =
         ContextCompat.checkSelfPermission(
@@ -203,48 +139,6 @@ class AlertMapFragment : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMyL
         }
     }
 
-    @SuppressLint("MissingPermission")
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            RESQUEST_CODE_LOCATION -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                map.isMyLocationEnabled = true
-            } else {
-                Toast.makeText(this, "Para activar la localizacion, en ajustas acepta los permisos.", Toast.LENGTH_SHORT).show()
-            }
-            CODIGO_PERMISO_GPS->
-                if (grantResults.isEmpty()) {
-                } else if (grantResults.first() == PackageManager.PERMISSION_GRANTED) {
-                    iniciarActualizacionesPosicion()
-                } else {
-                    // Permiso negado
-                    val dialogo = AlertDialog.Builder(this)
-                    dialogo.setMessage("Esta app requiere GPS, ¿Quieres habilitarlo manualmente?")
-                        .setPositiveButton("Aceptar", DialogInterface.OnClickListener { dialog, which ->
-                            val intent = Intent()
-                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                            val uri = Uri.fromParts(
-                                "package",
-                                BuildConfig.APPLICATION_ID, null
-                            )
-                            intent.data = uri
-                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                            startActivity(intent)
-                        })
-                        .setNeutralButton("Cancelar", DialogInterface.OnClickListener { dialog, which ->
-                            println("No hay forma de usar gps, cerrar la actividad")
-                            //Deshabilitar funcionalidad
-                        })
-                        .setCancelable(false)
-                    dialogo.show()
-                }
-            else->{}
-        }
-    }
 
     @SuppressLint("MissingPermission")
     override fun onResumeFragments() {
