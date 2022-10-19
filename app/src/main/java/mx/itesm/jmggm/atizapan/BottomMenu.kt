735674ce1b4616954900ee39b1d71f2482516b1a
André <84719490+gggandre@users.noreply.github.com>
2022-10-18 19:31:35 -0500

    mapa arreglado y login

--- app/src/main/java/mx/itesm/jmggm/atizapan/BottomMenu.kt
+++ app/src/main/java/mx/itesm/jmggm/atizapan/BottomMenu.kt
@@ -1,23 +1,37 @@
 package mx.itesm.jmggm.atizapan
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
 import mx.itesm.jmggm.atizapan.databinding.ActivityBottomMenuBinding
 
 class BottomMenu : AppCompatActivity() {
 
     private lateinit var binding: ActivityBottomMenuBinding
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityBottomMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener{ task: Task<String> ->
+            if(!task.isSuccessful){
+                return@addOnCompleteListener
+            }
+            val token = task.result
+            Log.d("PUSH_TOKEN", "pushToken: $token")
+
+        }
 
         val navView: BottomNavigationView = binding.navView
 
--- app/src/main/java/mx/itesm/jmggm/atizapan/ServiciosViewModel.kt
+++
@@ -1,7 +0,0 @@
-package mx.itesm.jmggm.atizapan
-
-import androidx.lifecycle.ViewModel
-
-class ServiciosViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
